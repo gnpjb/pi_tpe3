39a71adf7007c936867af0c86b312c11d6ec9bc3
guido barbieri <guido_gucci@hotmail.com>
2018-07-11 04:57:43 -0300

    Update cargarAeropuertos.c

--- cargarAeropuertos.c
+++ cargarAeropuertos.c
@@ -20,34 +20,34 @@ AeroListaADT cargarAeropuertos(char *filename){
 
 	char *field;
 	while(fgets(linea,MAX_LONG_LINEA,f)!=NULL){
+		setAeropuertoIATA(toAdd,"");
+		setAeropuertoLocal(toAdd,"");
+		setAeropuertoOACI(toAdd,"");
+		setAeropuertoDenominacion(toAdd,"");
 		//primer field es local
 		field=strtok(linea,DELIM);
 		if(field!=NULL){
 			setAeropuertoLocal(toAdd,field);
+		}
 			//segundo field es oaci, de que este exista depende si es valido o no
+		field=strtok(NULL,DELIM);
+		if(field!=NULL&&strlen(field)==4){
+			setAeropuertoOACI(toAdd,field);
+			//tercer field es iata
 			field=strtok(NULL,DELIM);
-			if(field!=NULL&&strlen(field)==4){
-				setAeropuertoOACI(toAdd,field);
-				//tercer field es iata
-				field=strtok(NULL,DELIM);
-				if(field!=NULL){
-					setAeropuertoIATA(toAdd,field);
-
-					//cuarto field es tipo
-					field=strtok(NULL,DELIM);
-					if(field!=NULL){
-						//quinto field es denominacion
-						field=strtok(NULL,DELIM);
-						if(field!=NULL){
-							setAeropuertoDenominacion(toAdd,field);
-							addAeroLista(resp,toAdd);
-
-						}
-
-					}
-
-				}
+			if(field!=NULL){
+				if(strlen(field)==3)
+				setAeropuertoIATA(toAdd,field);
 			}
+
+			//cuarto field es tipo
+			field=strtok(NULL,DELIM);
+			//quinto field es denominacion
+			field=strtok(NULL,DELIM);
+			if(field!=NULL){
+				setAeropuertoDenominacion(toAdd,field);
+			}
+				addAeroLista(resp,toAdd);
 		}
 	}
 	freeAeropuerto(toAdd);
